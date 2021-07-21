232374ce845ae16fe21e971fb97f33ffcbb197f5
Syama Rangapuram <rangapur@amazon.de>
2021-07-21 20:41:43 +0200

    update scripts to save results and show them later

---
+++ experiments/show_results.py
@@ -0,0 +1,42 @@
+import argparse
+
+from config.method_config import *
+import utils
+
+
+if __name__ == "__main__":
+    """
+    Prints results of methods that were already run:
+
+    python print_results.py --dataset dataset --method method --num-runs 5
+
+    where
+
+        dataset is one of: [labour, traffic, tourism, tourismlarge, wiki]
+        (see config/dataset_config.py)        
+
+        method is one of: [HierE2E, DeepVAR, DeepVARPlus, 
+                            ETS_NaiveBU, ARIMA_NaiveBU,
+                            ETS_MINT_shr, ETS_MINT_ols, ARIMA_MINT_shr, ARIMA_MINT_ols,
+                            ETS_ERM, ARIMA_ERM,
+                            PERMBU_MINT, 
+                          ]
+        (see config/method_config.py) 
+        
+        num-runs: number of runs to consider while taking the mean and std; default 5
+
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", type=str, required=True)
+    parser.add_argument("--method", type=str, required=True)
+    parser.add_argument("--num-runs", type=int, required=False, default=5)
+
+    args, _ = parser.parse_known_args()
+
+    dataset = args.dataset
+    method = args.method
+    num_runs = args.num_runs
+
+    agg_metrics_ls, level_wise_agg_metrics_ls = utils.parse_results(dataset=dataset, method=method, num_runs=num_runs)
+    utils.print_results(agg_metrics_ls=agg_metrics_ls, level_wise_agg_metrics_ls=level_wise_agg_metrics_ls)
---
+++ experiments/utils.py
@@ -0,0 +1,55 @@
+import glob
+import json
+import numpy as np
+import os
+import pickle
+
+
+def get_best_hps(dataset, method):
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    ix = cwd.rfind('/')
+    source_dir = cwd[: ix + 1]
+
+    best_hps_by_dataset = {}
+    with open(f'{source_dir}/experiments/best_hps/best_hps_{method}.jsonl') as fp:
+        for line in fp.readlines():
+            d = json.loads(line)
+            best_hps_by_dataset.update(json.loads(d))
+
+    return best_hps_by_dataset[dataset]
+
+
+def parse_results(dataset, method, num_runs):
+    results_path = f"./experiments/results/{method}/{dataset}"
+    result_files = glob.glob(f"{results_path}/*.pkl")
+
+    agg_metrics_ls, level_wise_agg_metrics_ls = [], []
+    num_runs = min(num_runs, len(result_files))
+    for file in result_files[:num_runs]:
+        with open(file, "rb") as fp:
+            agg_metrics, level_wise_agg_metrics = pickle.load(fp)
+        agg_metrics_ls.append(agg_metrics)
+        level_wise_agg_metrics_ls.append(level_wise_agg_metrics)
+
+    return agg_metrics_ls, level_wise_agg_metrics_ls
+
+
+def print_results(agg_metrics_ls, level_wise_agg_metrics_ls):
+    num_runs = len(agg_metrics_ls)
+
+    print(f"\n****** Results averaged over {num_runs} runs "
+          f"(level-wise CRPS scores are shown first followed by the overall CRPS score): ******")
+
+    for level_metric_name in level_wise_agg_metrics_ls[0].keys():
+        errors = [
+            level_wise_agg_metric[level_metric_name]
+            for level_wise_agg_metric in level_wise_agg_metrics_ls
+        ]
+        print(f"Mean +/- std. of {level_metric_name} over {num_runs} num_runs: {np.mean(errors)} +/- {np.std(errors)}")
+
+    metric_name = "mean_wQuantileLoss"
+    errors = [
+        agg_metrics[metric_name]
+        for agg_metrics in agg_metrics_ls
+    ]
+    print(f"Mean +/- std. of {metric_name} over {num_runs} num_runs: {np.mean(errors)} +/- {np.std(errors)}")
